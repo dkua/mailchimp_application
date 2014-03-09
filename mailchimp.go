e64597561d333e23a4cfdfc53a0e6f8fb454d1e6
David Kua <david@kua.io>
2014-03-09 17:31:01 -0400

    Finished mailchimp.go

--- mailchimp.go
+++ mailchimp.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-  "io/ioutil"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
+
+const BaseUrl string = "https://mandrillapp.com/api/1.0/"
+const SendEndpoint string = "messages/send.json"
 
 type Configuration struct {
 	Host     string
@@ -19,13 +24,14 @@ type Email struct {
 	Key     string  `json:"key"`
 	Message Message `json:"message"`
 }
+
 type Message struct {
 	Html        string       `json:"html"`
 	Text        string       `json:"text"`
 	Subject     string       `json:"subject"`
 	FromEmail   string       `json:"from_email"`
 	FromName    string       `json:"from_name"`
-	To          string       `json:"to"`
+	To          []To         `json:"to"`
 	Attachments []Attachment `json:"attachments"`
 }
 
@@ -33,6 +39,12 @@ type Attachment struct {
 	Type    string `json:"type"`
 	Name    string `json:"name"`
 	Content string `json:"content"`
+}
+
+type To struct {
+	Email string `json:"email"`
+	Name  string `json:"name"`
+	Type  string `json:"type"`
 }
 
 func getConfiguration() Configuration {
@@ -44,23 +56,23 @@ func getConfiguration() Configuration {
 }
 
 func createAttachment(mime, name, filepath string) Attachment {
-    file, err := ioutil.ReadFile(filepath)
-    if err != nil {
-        panic(err)
-    }
-    content := base64.StdEncoding.EncodeToString(file)
-    attachment := Attachment{
-        Type: mime,
-        Name: name,
-        Content: content,
-    }
-    return attachment
+	file, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		panic(err)
+	}
+	content := base64.URLEncoding.EncodeToString(file)
+	attachment := Attachment{
+		Type:    mime,
+		Name:    name,
+		Content: content,
+	}
+	return attachment
 }
 
 func main() {
 	configuration := getConfiguration()
 
-	attachments := make([]Attachment, 0)
+	attachments := make([]Attachment, 1)
 	attachment := createAttachment("text/plain", "mailchimp.go", "mailchimp.go")
 
 	if len(os.Args) != 2 {
@@ -69,12 +81,19 @@ func main() {
 		return
 	}
 
+	recipients := make([]To, 1)
+	recipient := To{
+		Email: os.Args[1],
+		Name:  "Mailchimp",
+		Type:  "to",
+	}
+
 	message := Message{
-		Html:        "Hey there, here is my Go code that uses the Mandrill API to send itself as an attachment in this email. I'm sending this as an application for your Software Engineer Intern position. For more of my code you can check out my Github: github.com/dkua. Cheers!",
-		Text:        "Hey there, here is my Go code that uses the Mandrill API to send itself as an attachment in this email. I'm sending this as an application for your Software Engineer Intern position. For more of my code you can check out my Github: github.com/dkua. Cheers!",
+		Html:        "Hey there, here is my Go code that uses the Mandrill API to send itself as an attachment in this email. I'm sending this as an application for your Software Engineer Intern position. The code for this can also be found on my Github at github.com/dkua/mandrill_application. Cheers!",
+		Text:        "Hey there, here is my Go code that uses the Mandrill API to send itself as an attachment in this email. I'm sending this as an application for your Software Engineer Intern position. The code for this can also be found on my Github at github.com/dkua/mandrill_application. Cheers!",
 		Subject:     "Code Sample - David Kua",
 		FromEmail:   configuration.Username,
-		To:          os.Args[1],
+		To:          append(recipients, recipient),
 		Attachments: append(attachments, attachment),
 	}
 
@@ -83,9 +102,20 @@ func main() {
 		Message: message,
 	}
 
-	req, err := json.Marshal(email)
+	content, err := json.Marshal(email)
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
-	fmt.Println(string(req))
+	fmt.Println(string(content))
+	resp, err := http.Post(BaseUrl+SendEndpoint, "application/json", bytes.NewBuffer(content))
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	defer resp.Body.Close()
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%s\n", string(contents))
 }
